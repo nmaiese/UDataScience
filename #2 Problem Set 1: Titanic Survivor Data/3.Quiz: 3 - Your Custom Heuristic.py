ab3633b93bf0029fccdf52a8f51e5f4971bd90b8
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-02-04 10:53:21 +0100

    initializate repository

---
+++ #2 Problem Set 1: Titanic Survivor Data/3.Quiz: 3 - Your Custom Heuristic.py
@@ -0,0 +1,98 @@
+import numpy
+import pandas
+import statsmodels.api as sm
+
+file_path = 'data/titanic-data.csv'
+
+
+def custom_heuristic(file_path):
+    '''
+    You are given a list of Titantic passengers and their associated
+    information. More information about the data can be seen at the link below:
+    http://www.kaggle.com/c/titanic-gettingStarted/data
+
+    For this exercise, you need to write a custom heuristic that will take
+    in some combination of the passenger's attributes and predict if the passenger
+    survived the Titanic diaster.
+
+    Can your custom heuristic beat 80% accuracy?
+
+    The available attributes are:
+    Pclass          Passenger Class
+                    (1 = 1st; 2 = 2nd; 3 = 3rd)
+    Name            Name
+    Sex             Sex
+    Age             Age
+    SibSp           Number of Siblings/Spouses Aboard
+    Parch           Number of Parents/Children Aboard
+    Ticket          Ticket Number
+    Fare            Passenger Fare
+    Cabin           Cabin
+    Embarked        Port of Embarkation
+                    (C = Cherbourg; Q = Queenstown; S = Southampton)
+
+    SPECIAL NOTES:
+    Pclass is a proxy for socioeconomic status (SES)
+    1st ~ Upper; 2nd ~ Middle; 3rd ~ Lower
+
+    Age is in years; fractional if age less than one
+    If the age is estimated, it is in the form xx.5
+
+    With respect to the family relation variables (i.e. SibSp and Parch)
+    some relations were ignored. The following are the definitions used
+    for SibSp and Parch.
+
+    Sibling:  brother, sister, stepbrother, or stepsister of passenger aboard Titanic
+    Spouse:   husband or wife of passenger aboard Titanic (mistresses and fiancees ignored)
+    Parent:   mother or father of passenger aboard Titanic
+    Child:    son, daughter, stepson, or stepdaughter of passenger aboard Titanic
+
+    Write your prediction back into the "predictions" dictionary. The
+    key of the dictionary should be the passenger's id (which can be accessed
+    via passenger["PassengerId"]) and the associating value should be 1 if the
+    passenger survvied or 0 otherwise.
+
+    For example, if a passenger is predicted to have survived:
+    passenger_id = passenger['PassengerId']
+    predictions[passenger_id] = 1
+
+    And if a passenger is predicted to have perished in the disaster:
+    passenger_id = passenger['PassengerId']
+    predictions[passenger_id] = 0
+
+    You can also look at the Titantic data that you will be working with
+    at the link below:
+    https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/titanic_data.csv
+    '''
+
+    predictions = {}
+    df = pandas.read_csv(file_path)
+    for passenger_index, passenger in df.iterrows():
+        passenger_id = passenger['PassengerId']
+        family_size = passenger['SibSp'] + passenger['Parch'] + 1
+        gender = passenger['Sex']
+        age = passenger['Age']
+        ses = passenger['Pclass']
+
+        if (gender == 'female' and family_size in [1, 2, 3]) or (ses in [1, 2] and age < 18):
+            predictions[passenger_id] = 1
+        else:
+            predictions[passenger_id] = 0
+
+    accurate(df, predictions)
+
+    return predictions
+
+
+def accurate(df, predictions):
+        results = 0
+        for passenger_index, passenger in df.iterrows():
+            passenger_id = passenger['PassengerId']
+            if predictions[passenger_id] == passenger['Survived']:
+                results += 1
+
+        score = float(results) * 100 / len(predictions)
+        print 'Your heuristic is '+ str(score) +'% accurate. Is it 80% or better?'
+
+        return
+
