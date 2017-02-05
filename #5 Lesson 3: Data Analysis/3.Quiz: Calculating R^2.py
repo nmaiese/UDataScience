b1b7dd34db1368737ab4aa591c8867ea0fc1e5f2
Aniello Maiese <aniello.maiese@mecglobal.com>
2017-02-05 17:31:37 +0100

    Started Lesson 3 Exercises

---
+++ #5 Lesson 3: Data Analysis/3.Quiz: Calculating R^2.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def compute_r_squared(data, predictions):
+    # Write a function that, given two input numpy arrays, 'data', and 'predictions,'
+    # returns the coefficient of determination, R^2, for the model that produced
+    # predictions.
+    #
+    # Numpy has a couple of functions -- np.mean() and np.sum() --
+    # that you might find useful, but you don't have to use them.
+
+    # YOUR CODE GOES HERE
+
+    data_mean = np.mean(data)
+    r_squared = 1 - (np.sum(np.square(data-predictions))/(np.sum(np.square(data-data_mean))))
+
+
+    return r_squared
